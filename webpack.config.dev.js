60693f85f679ac02dbef50a0427faf3d1b46e26c
吴下阿江 <780185916@qq.com>
2021-12-23 11:56:40 +0800

    uni app 支持 vue3

---
+++ webpack.config.dev.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir);
+}
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'uni-socket.io.js',
+    library: "uniSocketIo",
+    libraryTarget: "var"
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(/debug/, path.resolve(__dirname, './lib/support/debug.js')),
+    new webpack.NormalModuleReplacementPlugin(/^engine.io-client$/, path.resolve(__dirname, './lib/engine.io-client')),
+    new EsmWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('lib')],
+        options:{
+          presets:['env'] //转码规则
+        }
+      },
+    ],
+  },
+  mode: 'development',
+};
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'uni-socket.io.js',
+    libraryTarget: 'umd',
+  },
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(
+      /debug/,
+      path.resolve(__dirname, './lib/support/noop.js')
+    ),
+    new webpack.NormalModuleReplacementPlugin(
+      /^engine.io-client$/,
+      path.resolve(__dirname, './lib/engine.io-client')
+    ),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('lib')],
+        options:{
+          presets:['env'] //转码规则
+        }
+      },
+    ],
+  },
+  mode: 'production',
+}
